bd43622b8a41dc33b2fded25d71d15ab3a32061d
Snehlata Sarode <sarode.snehlata31@gmail.com>
2021-07-08 18:31:52 +0530

    added some log to debug

--- src/Actions/ProductActions.js
+++ src/Actions/ProductActions.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import Axios from "axios";
 import {
     PRODUCT_DETAILS_FAIL,
     PRODUCT_DETAILS_REQUEST,
@@ -12,33 +12,20 @@ export const listProducts = () => async (dispatch) => {
         type: PRODUCT_LIST_REQUEST
     })
     try {
-        const { data } = await axios.get(`/api/products`);
+        const { data } = await Axios.get(`/api/products`);
         dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
+        console.log("data in peoduct Action", data);
     }
     catch (error) {
         dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message })
     }
 }
-// export const detailsProduct = (productId) => async (dispatch) => {
-//     console.log("product in single data action")
-//     dispatch({
-//         type: PRODUCT_DETAILS_REQUEST, payload: productId
-//     })
-//     try {
-//         const { data } = await axios.get(`/api/products/${productId}`);
-//         dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
-//     }
-//     catch (error) {
-//         dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message })
-//     }
-// }
-
-
 export const detailsProduct = (productId) => async (dispatch) => {
     dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
     try {
         const { data } = await Axios.get(`/api/products/${productId}`);
         dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+
     } catch (error) {
         dispatch({
             type: PRODUCT_DETAILS_FAIL,
