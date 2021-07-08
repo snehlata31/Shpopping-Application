dfb96f45174eabdfbf5abbe70cb9522c7c9611c6
Snehlata Sarode <sarode.snehlata31@gmail.com>
2021-07-08 18:34:21 +0530

    use cartRedux and added item on cart

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import ProductScreen from './Screens/ProductScreen';
 import HomeScreen from './Screens/HomeScreen';
 import { BrowserRouter, Route } from "react-router-dom";
+import CartScreen from './Screens/CartScreen';
+import { Link } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
 const App = () => {
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand">amazona</a>
+            <Link className="brand" to="/">
+              amazona
+            </Link>
           </div>
           <div>
-            <a href="cart.html">Cart</a>
+            <a href="cart.html">Cart
+              {
+                cartItems.length > 0 && (
+
+                  <span className="badge">{cartItems.length}</span>
+                )
+              }
+            </a>
             <a href="sighin.html">Sigh In</a>
           </div>
         </header>
         <main>
+          <Route path="/cart/:id?" component={CartScreen} exact></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All Right preserved</footer>
       </div>
     </BrowserRouter >
-
   );
 }
 
