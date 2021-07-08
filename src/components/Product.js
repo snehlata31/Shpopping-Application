9004c53a98c5ab5558e272dd0ec8a88d8c66f6aa
Snehlata Sarode <sarode.snehlata31@gmail.com>
2021-07-08 18:49:38 +0530

    added Link to all acchro tag

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Rating from './Rating ';
+import { Link } from 'react-router-dom';
 const Product = (props) => {
     console.log(props);
     const { product } = props;
@@ -7,19 +8,22 @@ const Product = (props) => {
     return (
         <div>
             <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                    <img src={product.image} className="medium" alt={product.name} />
-                </a>
+                <Link to={`/product/${product._id}`}>
+                    <img className="medium" src={product.image} alt={product.name} />
+
+                </Link>
                 <div className="card-body">
-                    <a href={`/product/${product._id}`}>
-                        <h2>{product.name}</h2></a>
+                    <Link to={`/product/${product._id}`}>
+                        <h2>{product.name}</h2>
+
+                    </Link>
                     <Rating rating={product.rating}
                         numReviews={product.numReviews}>
                     </Rating>
                     <div className="price">{product.price}</div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
 export default Product;
